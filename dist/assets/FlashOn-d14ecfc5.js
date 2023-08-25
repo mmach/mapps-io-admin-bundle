f88231d7449dfae24fd23e6828bb3e8ea67a08bd
RXS\mmach-c <mmach-c@rxsinfo.com>
2023-08-25 13:45:06 +0200

    v1

---
+++ dist/assets/FlashOn-d14ecfc5.js
@@ -0,0 +1,1 @@
+import{b as r,Y as a,i as t,Z as l}from"./index-085b2e83.js";var e={},i=t,o=l;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,d=o(r),v=i(a()),p=(0,v.default)(d.createElement("path",{d:"M7 2v11h3v9l7-12h-4l4-8z"}),"FlashOn");u=e.default=p;export{u as d};
