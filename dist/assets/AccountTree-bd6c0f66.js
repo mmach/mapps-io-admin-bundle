4fd2e6490c62aedbfdf9cdf126a0f13f2d042bb6
RXS\mmach-c <mmach-c@rxsinfo.com>
2023-09-05 11:30:16 +0200

    v6

--- dist/assets/AccountTree-bd6c0f66.js
+++
@@ -1,1 +0,0 @@
-import{b as r,Y as a,i as t,Z as v}from"./index-085b2e83.js";var e={},o=t,u=v;Object.defineProperty(e,"__esModule",{value:!0});var i=e.default=void 0,d=u(r),c=o(a()),l=(0,c.default)(d.createElement("path",{d:"M22 11V3h-7v3H9V3H2v8h7V8h2v10h4v3h7v-8h-7v3h-2V8h2v3z"}),"AccountTree");i=e.default=l;export{i as d};
